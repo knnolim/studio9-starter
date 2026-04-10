91c6632cf9103c4460c8b598d5db604e2afb4b84
Kene Nnolim <knnolim@mit.edu>
2026-04-10 14:47:22 -0400

    Done

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ import {
   useGraffitiDiscover,
 } from "@graffiti-garden/wrapper-vue";
 
+import {reactive, computed} from "vue";
+
 function setup() {
   // Initialize Graffiti (you'll need this later)
   const graffiti = useGraffiti();
@@ -17,27 +19,59 @@ function setup() {
   const myMessage = ref("");
 
   // Declare a signal representing the messages in the chat
-  const messageObjects = ref([]);
-
-  function sendMessage() {
-    messageObjects.value.push({
+  const { objects: messageObjects } = useGraffitiDiscover(
+    [ `designftw-26-studio-1` ],
+    {
       value: {
-        content: myMessage.value,
-        published: Date.now(),
+        required: [ 'content', 'published' ],
+          properties: {
+            content: { type: "string" },
+            published: { type: "number" },
+          },
       },
-    });
+    },
+    undefined, // If you wanted private messages, this is where you'd supply a "session"
+      true // automatically poll for new messages
+  );
+
+  const sortedMessageObjects = computed(() => {
+    return messageObjects.value.toSorted((a, b) => 
+      b.value.published - a.value.published);
+  })
+
+  const isSending = ref(false);
+
+  async function sendMessage() {
+    isSending.value = true;
+    // await new Promise((resolve) => setTimeout(resolve, 1000));
+    
+    await graffiti.post(
+      {
+          value: {
+            content: myMessage.value,
+            published: Date.now(),
+          },
+          channels: [ `designftw-26-studio-1` ]
+      },
+      session.value,
+    );
+
+    myMessage.value = "";
+    isSending.value = false;
   }
 
   return {
     myMessage,
     messageObjects,
+    sortedMessageObjects,
     sendMessage,
+    isSending
   };
 }
 
 createApp({ template: "#template", setup })
   .use(GraffitiPlugin, {
-    graffiti: new GraffitiLocal(),
-    // graffiti: new GraffitiDecentralized(),
+    // graffiti: new GraffitiLocal(),
+    graffiti: new GraffitiDecentralized(),
   })
   .mount("#app");
